3cf19a48a7ae3205fde312de07a9d74f44faf048
denamolio <71786326+dacadla@users.noreply.github.com>
2023-09-08 14:22:51 +0200

    change name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="bg-primary text-white p-4">
-      <h1 className="text-3xl font-bold">Radovan Mihalik</h1>
+      <h1 className="text-3xl font-bold">denamolio</h1>
       {/* Add your small bio here */}
     </header>
   );
