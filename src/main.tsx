eef1b3774e6101dbeb7a0af63d006a5e9a63ffd3
denamolio <71786326+dacadla@users.noreply.github.com>
2023-09-08 11:46:16 +0200

    Fix import casing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '../styles/index.css'
-import Routes from './routes';
+import Routes from './Routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
